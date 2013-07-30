265ee3779fdea18b764bddf46662c3d771059bb2
Raoul <Raoul@RaoulsPC>
2013-07-31 01:30:59 +1200

    server working.need to properly send files

--- SE 325 Assignment 1/src/SE325Assignment1/TCPServer.java
+++ SE 325 Assignment 1/src/SE325Assignment1/TCPServer.java
@@ -20,7 +20,7 @@ import java.util.Date;
 
 /**
  * @author Raoul D'Cunha
- *
+ * 
  */
 public class TCPServer {
 
@@ -31,84 +31,81 @@ public class TCPServer {
 		// TODO Auto-generated method stub
 		runserver();
 	}
-	
-	private static void runserver()
-	{
+
+	private static void runserver() {
 		try {
-			ServerSocket socket = new ServerSocket( 0 );
-			InetAddress serverHost = InetAddress.getLocalHost( );
-			System.out.println( "Server destination: " + serverHost.getHostAddress( ) + ", "+ socket.getLocalPort( ) );
+			ServerSocket socket = new ServerSocket(0);
+			InetAddress serverHost = InetAddress.getLocalHost();
+			System.out.println("Server destination: "
+					+ serverHost.getHostAddress() + ", "
+					+ socket.getLocalPort());
 			/* Repeatedly handle requests for processing. */
-			while( true ) {
-				Socket clientConnection = socket.accept( );
-				BufferedReader d = new BufferedReader(new InputStreamReader(clientConnection.getInputStream( )));
-				
-				
-				DataOutputStream out = new DataOutputStream( clientConnection.getOutputStream( ) );
-				
-				DateFormat df = new SimpleDateFormat("EEE, dd MM yy HH:mm:ss zzz");
-				String formattedDate = df.format(new Date()); 
-				
-				//String output = d.readLine();
-				
-				String output = "HTTP/1.0 200 ok\r\nDate: " + 
-						formattedDate + 
-						"\r\nServer: SE325 Assignment 1 Server (rdcu001)\r\n" + 
-						"Content-type: text/html";
-				
-				
-				
-				String path = d.readLine().split(" ")[1];
+			while (true) {
+				Socket clientConnection = socket.accept();
+				BufferedReader d = new BufferedReader(new InputStreamReader(
+						clientConnection.getInputStream()));
+				String temp = d.readLine();
+				System.out.print(temp);
+
+				String path = temp.split(" ")[1];
+
+				path = System.getProperty("user.dir") + path;
 				String extension = "";
 
 				int i = path.lastIndexOf('.');
 				if (i > 0) {
-				    extension = path.substring(i+1);
+					extension = path.substring(i + 1);
 				}
-				
-				File f = new File(path);
-				try {
-					FileInputStream fs = new FileInputStream(f);
-					int end = 0;
-					while (end != -1)
-					{
-						out.write(fs.read());
+				System.out.print(extension);
+
+				if (extension.equals("html")) {
+					DataOutputStream out = new DataOutputStream(
+							clientConnection.getOutputStream());
+
+					DateFormat df = new SimpleDateFormat(
+							"EEE, dd MM yy HH:mm:ss zzz");
+					String formattedDate = df.format(new Date());
+
+					// String output = d.readLine();
+
+					String output = "HTTP/1.0 200 ok\r\nDate: "
+							+ formattedDate
+							+ "\r\nServer: SE325 Assignment 1 Server (rdcu001)\r\n"
+							+ "Content-type: text/html";
+
+					File f = new File(path);
+					try {
+						FileInputStream fs = new FileInputStream(f);
+						int end = 0;
+						while (end != -1) {
+							out.write(fs.read());
+						}
+
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-					
-					
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+					/**
+					 * output+="\r\n\r\n<html>\r\n" +
+					 * "<header><title>Test</title></header>\r\n" + "<body>\r\n"
+					 * + "Hello world\r\n" + "</body>\r\n" + "</html>";
+					 **/
+					System.out.println(output);
+
+					out.writeBytes(output);
+
+					/* Close this connection. */
+
 				}
-				
-				
-				
-				
-				
-				
-				/**
-				output+="\r\n\r\n<html>\r\n" + 
-						"<header><title>Test</title></header>\r\n" + 
-						"<body>\r\n" + 
-						"Hello world\r\n" + 
-						"</body>\r\n" + 
-						"</html>";
-				
-				**/
-				System.out.println(output);
-			
-				out.writeBytes(output);
-				
-				
-				/* Close this connection. */
-				clientConnection.close( );
+
+				clientConnection.close();
+
 			}
-			//socket.close();
-		}
-		catch( IOException e ) {
-			e.printStackTrace( );
+			// socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
 
 }
