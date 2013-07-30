17661c166763dc23909949c305eebed6665f1153
Raoul <Raoul@RaoulsPC>
2013-07-30 23:43:08 +1200

    created initial class

---
+++ SE 325 Assignment 1/src/SE325Assignment1/TCPServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package SE325Assignment1;
+
+/**
+ * @author Raoul
+ *
+ */
+public class TCPServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
