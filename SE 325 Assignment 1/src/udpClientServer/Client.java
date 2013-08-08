29cc296887fa36debc06d3f68e2d11d07a97e739
Raoul <raoul_dcunha@hotmail.com>
2013-08-08 15:28:22 +1200

    added code from lecture slides

--- SE 325 Assignment 1/src/udpClientServer/Client.java
+++ SE 325 Assignment 1/src/udpClientServer/Client.java
@@ -1,13 +1,47 @@
 package udpClientServer;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			InetAddress serverAddress = InetAddress.getByName( args[ 0 ] );
+			int serverPort = Integer.parseInt( args[ 1 ] );
+			DatagramSocket socket = new DatagramSocket();
+			int x = Integer.parseInt( args[ 2 ] );
+			int y = Integer.parseInt( args[ 3 ] );
+			ByteArrayOutputStream outputByteStream = new ByteArrayOutputStream( );
+			DataOutputStream outputDataStream = new DataOutputStream( outputByteStream );
+			outputDataStream.writeInt( x );
+			outputDataStream.writeInt( y );
+			byte[ ] contents = outputByteStream.toByteArray( );
+			DatagramPacket request = new DatagramPacket( contents, contents.length, serverAddress, serverPort );
+			socket.send( request );
+			byte[ ] buffer = new byte[ 4 ];
+			DatagramPacket reply = new DatagramPacket( buffer, buffer.length );
+			socket.receive( reply );
+			ByteArrayInputStream inputByteStream = new ByteArrayInputStream( buffer );
+			DataInputStream inputDataStream = new DataInputStream( inputByteStream );
+			int product = inputDataStream.readInt( ); 
+			System.out.println( "Received: " + product );
+			socket.close( );
+		}
+		catch( NumberFormatException e ) {
+			e.printStackTrace( );
+		}
+		catch( IOException e ) {
+			e.printStackTrace( );
+		}
 	}
-
 }
--- SE 325 Assignment 1/src/udpClientServer/Server.java
+++ SE 325 Assignment 1/src/udpClientServer/Server.java
@@ -1,12 +1,44 @@
 package udpClientServer;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Server {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			DatagramSocket socket = new DatagramSocket( );
+			InetAddress serverHost = InetAddress.getLocalHost( );
+			System.out.println( "Server destination: " + serverHost.getHostAddress() + ", " + socket.getLocalPort( ) );
+			byte[ ] buffer = new byte[ 8 ];
+			while( true ) {
+				DatagramPacket request = new DatagramPacket( buffer, buffer.length );
+				socket.receive( request );
+				ByteArrayInputStream inputByteStream = new ByteArrayInputStream( buffer );
+				DataInputStream inputDataStream = new DataInputStream( inputByteStream );
+				int x = inputDataStream.readInt();
+				int y = inputDataStream.readInt();
+				int product = x * y;
+				ByteArrayOutputStream outputByteStream = new ByteArrayOutputStream();
+				DataOutputStream outputDataStream = new DataOutputStream( outputByteStream );
+				outputDataStream.writeInt( product );
+				byte[ ] contents = outputByteStream.toByteArray( );
+				DatagramPacket response = new DatagramPacket( contents, contents.length, request.getAddress( ), request.getPort( ) );
+				socket.send( response );
+			}
+		}
+		catch( IOException e ) {
+			e.printStackTrace();
+		}
 
 	}
 
